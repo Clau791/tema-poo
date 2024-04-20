767f6e3425e70e20639dbdafac63bead6aa969fa
Ceausescu Claudiu <klau2015.cc@gmail.com>
2024-04-20 21:39:47 +0300

    test

--- include/Oras.h
+++ include/Oras.h
@@ -1,67 +1,26 @@
-//
-// Created by klau2 on 10.04.2024.
-//
 #ifndef OOP_ORAS_H
 #define OOP_ORAS_H
+
 #include <iostream>
-#include <utility>
 #include <vector>
 #include <string>
-//#include <Camera.h>
-#include <Hotel.h>
-
+#include "Hotel.h" // Asigurați-vă că includeți corect header-ul
 
 class Oras {
 private:
     std::string nume;
-
-    //Initializam vectorul pentru a stoca și gestiona hotelurile
     std::vector<Hotel> hoteluri;
 
 public:
+    explicit Oras(std::string nume = "");
+    [[nodiscard]] const std::string& getNume() const;
+    [[nodiscard]] const std::vector<Hotel>& getHoteluri() const;
+    void adaugaHotel(const Hotel& hotel);
 
-    //Constructor pentru Oras
-    explicit Oras(std::string  nume = "") : nume(std::move(nume)) {
+    friend std::ostream& operator<<(std::ostream& out, const Oras& oras);
+    friend std::istream& operator>>(std::istream& in, Oras& oras);
 
-    }
-
-    [[nodiscard]] const std::string& getNume() const {
-        return nume;
-    }
-
-    // Adaugam un getter pentru hoteluri
-    [[nodiscard]] const std::vector<Hotel>& getHoteluri() const {
-        return hoteluri;
-    }
-
-    void adaugaHotel(const Hotel& hotel) {
-        hoteluri.push_back(hotel);
-    }
-
-    friend std::ostream& operator<<(std::ostream& out, const Oras& oras) {
-        out << "Oras: " << oras.nume << "\n";
-        for (const auto& hotel : oras.hoteluri) {
-            out << hotel;
-        }
-        return out;
-    }
-
-    friend std::istream& operator>>(std::istream& in, Oras& oras) {
-        std::cout << "Nume Oras: ";
-        in >> oras.nume;
-        int nrHoteluri;
-        std::cout << "Nr hoteluri: ";
-        in >> nrHoteluri;
-        Hotel temp;
-        for (int i = 0; i < nrHoteluri; i++) {
-            std::cout << "Hotel " << i + 1 << ": ";
-            in >> temp;
-            oras.hoteluri.push_back(temp);
-        }
-        return in;
-    }
-    ~Oras() =default;
+    virtual ~Oras();
 };
 
-
-#endif //OOP_ORAS_H
+#endif // OOP_ORAS_H
--- src/Oras.cpp
+++ src/Oras.cpp
@@ -1,3 +1,43 @@
 //
 // Created by klau2 on 20.04.2024.
 //
+#include "Oras.h"
+
+Oras::Oras(std::string nume) : nume(std::move(nume)) {}
+
+const std::string& Oras::getNume() const {
+    return nume;
+}
+
+const std::vector<Hotel>& Oras::getHoteluri() const {
+    return hoteluri;
+}
+
+void Oras::adaugaHotel(const Hotel& hotel) {
+    hoteluri.push_back(hotel);
+}
+
+std::ostream& operator<<(std::ostream& out, const Oras& oras) {
+    out << "Oras: " << oras.nume << "\n";
+    for (const auto& hotel : oras.hoteluri) {
+        out << hotel;
+    }
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Oras& oras) {
+    std::cout << "Nume Oras: ";
+    in >> oras.nume;
+    int nrHoteluri;
+    std::cout << "Nr hoteluri: ";
+    in >> nrHoteluri;
+    Hotel temp;
+    for (int i = 0; i < nrHoteluri; i++) {
+        std::cout << "Hotel " << i + 1 << ": ";
+        in >> temp;
+        oras.hoteluri.push_back(temp);
+    }
+    return in;
+}
+
+Oras::~Oras() = default;
