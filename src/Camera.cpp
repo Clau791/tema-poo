10aa9c7ccf95c28f1601aaab55f6d0c3cc59d1f3
Clau791 <163884275+Clau791@users.noreply.github.com>
2024-04-20 20:25:21 +0300

    Update Camera.cpp

---
+++ src/Camera.cpp
@@ -0,0 +1,43 @@
+#include "Camera.h"
+
+int Camera::reducereGlobala = 0;
+int Camera::camereOcupate = 0;
+int SumaDePlata = 0;
+
+Camera::Camera(int numar, std::string tip, int pretPeNoapte, bool esteOcupata)
+    : numar(numar), tip(std::move(tip)), pretPeNoapte(pretPeNoapte), esteOcupata(esteOcupata) {}
+
+Camera::Camera(const Camera& sursa)
+    : numar(sursa.numar), tip(sursa.tip), pretPeNoapte(sursa.pretPeNoapte), esteOcupata(sursa.esteOcupata) {}
+
+void Camera::actualizeazaReducere() {
+    if (camereOcupate >= 2 && camereOcupate < 3) {
+        reducereGlobala = 50;
+    } else if (camereOcupate >= 3) {
+        reducereGlobala = 100;
+    }
+    std::cout << "Reducerea actualizata este: " << reducereGlobala << " RON" << std::endl;
+}
+
+void Camera::SetterOcupata() {
+    esteOcupata = !esteOcupata;
+}
+
+void Camera::AfiseazaDetalii() {
+    std::cout << "Camera " << numar << ": " << tip << ", " << pretPeNoapte << " RON, "
+              << (esteOcupata ? "Ocupata" : "Libera") << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& out, const Camera& camera) {
+    out << "Nr: " << camera.numar << ", Tip: " << camera.tip
+        << ", Pret: " << camera.pretPeNoapte << ", "
+        << (camera.esteOcupata ? "Ocupata" : "Libera");
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Camera& camera) {
+    in >> camera.numar >> camera.tip >> camera.pretPeNoapte >> camera.esteOcupata;
+    return in;
+}
+
+Camera::~Camera() {}
