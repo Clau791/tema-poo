fdd2fdcdab0a3fd2e3069b3ad1f3a6de0b8f8f3d
Ceausescu Claudiu <klau2015.cc@gmail.com>
2024-04-20 15:09:00 +0300

    test

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 // separarea codului din clase în `.h`
 #include <Hotel.h>
 #include <Oras.h>
-#include "Camera.cpp"
+#include <camera.h>
+
+
+
 
 int main() {
 
@@ -29,17 +32,37 @@ int main() {
 
 
     // Crearea a 3 hoteluri
+    Hotel hotel1("Grand Plaza");
 
+    Camera camera1(101, "Dubla", 300.0);
+    Camera camera2(102, "Single", 300.0);
+    Camera camera3(103, "Dubla", 300.0, true);
+    Camera camera4(104, "Dubla", 300.0);
+    Camera camera5(105, "Dubla", 300.0, true);
     hotel1.adaugaCamera(camera1);
     hotel1.adaugaCamera(camera2);
     hotel1.adaugaCamera(camera3);
     hotel1.adaugaCamera(camera4);
     hotel1.adaugaCamera(camera5);
 
+
+    Hotel hotel2("Sea View");
+    Camera camera22(202, "Single", 300.0);
+    Camera camera23(203, "Dubla", 300.0, true);
+    Camera camera24(204, "Tripla", 300.0, true);
+    Camera camera25(205, "Tripla", 300.0);
     hotel2.adaugaCamera(camera22);
     hotel2.adaugaCamera(camera23);
     hotel2.adaugaCamera(camera24);
     hotel2.adaugaCamera(camera25);
+
+    Hotel hotel3("Grand Hotel");
+    Camera camera30(301, "Dubla", 300.0);
+    Camera camera31(302, "Single", 300.0);
+    Camera camera32(303, "Dubla", 300.0, true);
+    Camera camera33(304, "Dubla", 300.0, true);
+    Camera camera34(305, "Dubla", 300.0, true);
+    Camera camera35(305, "Tripla", 300.0);
 
     hotel3.adaugaCamera(camera30);
     hotel3.adaugaCamera(camera31);
@@ -48,6 +71,9 @@ int main() {
     hotel3.adaugaCamera(camera34);
     hotel3.adaugaCamera(camera35);
 
+    // Crearea unui oraș si adaugarea hotelurilor în oraș
+    Oras oras1("Constanta");
+    Oras oras2("Bucuresti");
     oras1.adaugaHotel(hotel1);
     oras1.adaugaHotel(hotel2);
     oras2.adaugaHotel(hotel3);
