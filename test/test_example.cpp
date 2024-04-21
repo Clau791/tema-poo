852695d974f8c2a75277482d0c24ae1dc6ee3e5e
Ceausescu Claudiu <klau2015.cc@gmail.com>
2024-04-21 20:10:48 +0300

    test

--- test/test_example.cpp
+++ test/test_example.cpp
@@ -6,17 +6,3 @@ TEST(SuiteName, TestName) {
     // Other assertions: https://google.github.io/googletest/reference/assertions.html#boolean
 }
 
-// Test pentru constructorul CameraStandard
-TEST(CameraTest, ConstructorTest) {
-    Camera camera(1, "Dubla", 200, false);
-    ASSERT_EQ(camera.getNumar(), 1);
-    ASSERT_EQ(camera.getPret(), 200);
-    ASSERT_FALSE(camera.getEsteOcupata());
-}
-
-// Test pentru setter-ul de stare ocupată
-TEST(CameraTest, SetterOcupataTest) {
-    Camera camera(1, "Dubla", 100, false);
-    camera.SetterOcupata();
-    ASSERT_TRUE(camera.getEsteOcupata());
-}
